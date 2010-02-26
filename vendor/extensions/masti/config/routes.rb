a0086c2e314de8723856eb125d412b90975acd06
settu <arumugasettu@gmail.com>
2010-02-26 22:09:13 +0530

    changed UI for home page

--- vendor/extensions/masti/config/routes.rb
+++ vendor/extensions/masti/config/routes.rb
@@ -1,4 +1,5 @@
 # Put your extension routes here.
+map.home '/', :controller => 'home'
 map.root :controller => "home", :action => "index"
 map.connect "update_assets", :controller => "home", :action => "update_assets"
 
