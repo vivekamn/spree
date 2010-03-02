fc5940e47916efa60ac5c90bbcf2f883ad3070f9
Ramanavel Selvaraju <ramanainfotech@gmail.com>
2010-03-02 10:15:18 +0530

    header pages added and get featured page added

--- vendor/extensions/masti/app/controllers/home_controller.rb
+++ vendor/extensions/masti/app/controllers/home_controller.rb
@@ -1,10 +1,41 @@
 class HomeController < Spree::BaseController
 
+#   before_filter :require_user,:only=>[:get_featured]
   def index
     deal = DealHistory.find(:first, :conditions =>['is_active = ?', true])  
     @featured_product = Product.find(:first, :conditions => ['id = ?',deal.product_id])
   end
+  
+  def contact_us
+    
+  end
+  
+  def get_featured
+    
+  end    
+  
+  def create
+    @enquiry=Enquiry.new(params[:enquiry])
+    if @enquiry.save
+      redirect_to home_url
+    else
+      redirect_to get_featured_path
+    end
 
+  end
+  
+  def about_us
+    
+  end
+  
+  def how_masti_works
+    
+  end
+  
+  def faq
+    
+  end
+  
   def update_assets
     # copy the assets from extensions public dir into #{RAILS_ROOT}/public
     destination = "#{RAILS_ROOT}/public"
--- vendor/extensions/masti/config/routes.rb
+++ vendor/extensions/masti/config/routes.rb
@@ -2,6 +2,11 @@
 map.home '/', :controller => 'home'
 map.root :controller => "home", :action => "index"
 map.connect "update_assets", :controller => "home", :action => "update_assets"
+map.about_us '/about-us',:controller => "home", :action => "about_us"
+map.how_masti_works '/how-masti-works',:controller => "home", :action => "how_masti_works"
+map.faq '/faq',:controller => "home", :action => "faq"
+map.get_featured '/get-featured',:controller=>"home",:action=>"get_featured"
+map.contact_us '/contact-us',:controller=>"home",:action=>"contact_us"
 
 # map.namespace :admin do |admin|
 #   admin.resources :whatever
