1adac1e70a050ebebb63b9153d3d3ea062b4017a
Ramanavel Selvaraju <ramanainfotech@gmail.com>
2010-03-05 18:31:00 +0530

    UI changes

--- config/environment.rb
+++ config/environment.rb
@@ -77,11 +77,7 @@ Spree::Initializer.run do |config|
   # The internationalization framework can be changed to have another default locale (standard is :en) or more load paths.
   # All files from config/locales/*.rb,yml are added automatically.
   #config.i18n.load_path << Dir[File.join(RAILS_ROOT, 'my', 'locales', '*.{rb,yml}')]
-#  config.i18n.default_locale = :'en-US'
-
-# config.active_record.default_timezone = 'Chennai'
-#   config.active_record.default_timezone = 'UTC'
-#  config.time_zone = 'UTC'
+  config.i18n.default_locale = :'en-US'
 
 end
 
