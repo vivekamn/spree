7eb5bd6f0f1e9538b1a91a76a57696776fc1a836
Ramanavel Selvaraju <ramanainfotech@gmail.com>
2010-03-05 14:28:28 +0530

    time zone changed

--- config/environment.rb
+++ config/environment.rb
@@ -63,7 +63,7 @@ Spree::Initializer.run do |config|
   # Make Time.zone default to the specified zone, and make Active Record store time values
   # in the database in UTC, and return them converted to the specified local zone.
   # Run "rake -D time" for a list of tasks for finding time zone names.
-  config.time_zone = "Eastern Time (US & Canada)"
+#  config.time_zone = "Eastern Time (US & Canada)"
 
   # Use SQL instead of Active Record's schema dumper when creating the test database.
   # This is necessary if your schema can't be completely dumped by the schema dumper,
@@ -77,12 +77,16 @@ Spree::Initializer.run do |config|
   # The internationalization framework can be changed to have another default locale (standard is :en) or more load paths.
   # All files from config/locales/*.rb,yml are added automatically.
   #config.i18n.load_path << Dir[File.join(RAILS_ROOT, 'my', 'locales', '*.{rb,yml}')]
-  config.i18n.default_locale = :'en-US'
+#  config.i18n.default_locale = :'en-US'
+
+# config.active_record.default_timezone = 'Chennai'
+#   config.active_record.default_timezone = 'UTC'
+#  config.time_zone = 'UTC'
 
 end
 
-Time::DATE_FORMATS[:date_time24] = "%Y-%m-%d %H:%M"
-Time::DATE_FORMATS[:short_date] = "%Y-%m-%d"
+#Time::DATE_FORMATS[:date_time24] = "%Y-%m-%d %H:%M"
+#Time::DATE_FORMATS[:short_date] = "%Y-%m-%d"
 
 # Add new inflection rules using the following format
 # (all these examples are active by default):
