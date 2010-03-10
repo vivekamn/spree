69a2800530e8d939b4557807a8c26f9e1771a305
Ramanavel Selvaraju <ramanainfotech@gmail.com>
2010-03-10 14:54:34 +0530

    vouvher added for orders

---
+++ vendor/extensions/masti/app/helpers/users_helper.rb
@@ -0,0 +1,22 @@
+module UsersHelper       
+  def password_style(user)
+    show_openid ? "display:none" : ""
+  end         
+  def openid_style(user) 
+    show_openid ? "": "display:none"
+  end
+  
+   def admin?
+    if current_user==User.first(:include => :roles, :conditions => ["roles.name = 'admin'"])
+      return true
+    else
+      return false
+    end
+    
+  end
+  
+  private 
+  def show_openid
+    Spree::Config[:allow_openid] and @user.openid_identifier
+  end
+end
