2799eabef37f22c6c3db3121505f340e93e1b2b7
madan <madan.innovative@gmail.com>
2010-03-11 19:27:58 +0530

    dollar to rupees and including discount

--- vendor/extensions/masti/app/helpers/orders_helper.rb
+++ vendor/extensions/masti/app/helpers/orders_helper.rb
@@ -1,3 +1,17 @@
 module OrdersHelper  
+  
+  def order_price(order, options={})
+    options.assert_valid_keys(:format_as_currency, :show_vat_text, :show_price_inc_vat)
+    options.reverse_merge! :format_as_currency => true, :show_vat_text => true
+
+    # overwrite show_vat_text if show_price_inc_vat is false
+    options[:show_vat_text] = Spree::Config[:show_price_inc_vat]
+
+    amount =  order.item_total
+    amount += Calculator::Vat.calculate_tax(order) if Spree::Config[:show_price_inc_vat]
+    amount = "Rs. "+amount.to_s
+    amount
+    #options.delete(:format_as_currency) ? number_to_currency(amount) : amount
+  end
 
 end
