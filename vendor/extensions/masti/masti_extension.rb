1ea7bc7eedbb25371ea1a8026e6d78c3076ab38e
settu <arumugasettu@gmail.com>
2010-03-12 12:54:33 +0530

    EBS files added

--- vendor/extensions/masti/masti_extension.rb
+++ vendor/extensions/masti/masti_extension.rb
@@ -40,6 +40,26 @@ class MastiExtension < Spree::Extension
                                                           :small => '100x100>', 
                                                           :product => '240x240>',
                                                           :large => '400x400>'}
+
+    UsersController.class_eval do
+      def res
+        require 'RubyRc4.rb'
+        require 'base64'
+        @key = 'ebskey'
+        @DR = params[:DR]
+        @DR.gsub!(/ /,'+')
+        @encrypted_data = Base64.decode64(@DR)
+        @decryptor = RubyRc4.new(@key)
+        @plain_text = @decryptor.encrypt(@encrypted_data)
+        puts "HTTP/1.0 200 OK"
+        puts "Content-type: text/html\n\n"
+        @plain_text.split(/&/).each_with_index do |item, i|
+          key, val = item.split(/=/)
+          puts "#{key}=#{val}"
+        end
+      end
+    end
+
     
 
 
