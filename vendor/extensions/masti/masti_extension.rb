d7a811b597b75d6fed26c062ddb7c3434a8657f0
Ramanavel Selvaraju <ramanainfotech@gmail.com>
2010-03-18 16:20:36 +0530

    index changes

--- vendor/extensions/masti/masti_extension.rb
+++ vendor/extensions/masti/masti_extension.rb
@@ -38,6 +38,13 @@ class MastiExtension < Spree::Extension
       validates_presence_of :vendor_id
       delegate_belongs_to :master, :count_on_hand
     end 
+    
+     User.class_eval do
+      attr_accessible :phone_no
+      validates_presence_of :phone_no
+      validates_numericality_of :phone_no
+      validates_length_of :phone_no, :is=>10
+    end 
 
     Image.attachment_definitions[:attachment][:styles] = {:mini => '48x48>', 
                                                           :small => '100x100>', 
