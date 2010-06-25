eab5041875d0f51fdb8cd403e85446c6769e675f
settu <arumugasettu@gmail.com>
2010-06-25 22:48:49 +0530

    default city is chennai

--- vendor/extensions/masti/db/migrate/20100617174531_add_city_id.rb
+++ vendor/extensions/masti/db/migrate/20100617174531_add_city_id.rb
@@ -1,13 +1,13 @@
 class AddCityId < ActiveRecord::Migration
   def self.up
-    add_column :products,:city_id,:integer
-    add_column :orders,:city_id,:integer
-    add_column :deal_histories,:city_id,:integer
+    add_column :products, :city_id, :integer, :default => 1
+    add_column :orders,:city_id,:integer, :default => 1
+    add_column :deal_histories,:city_id,:integer, :default => 1
   end
 
   def self.down
-    remove_column :products,:city_id
-    remove_column :orders,:city_id
-    remove_column :deal_histories,:city_id
+    remove_column :products, :city_id
+    remove_column :orders, :city_id
+    remove_column :deal_histories, :city_id
   end
 end
