1e0a17834eb6d128d4a2b4b7bd529821ade63813
rajasekari2i <ranjeetrajasekar@gmail.com>
2010-07-31 15:20:08 +0530

    added global contant

---
+++ vendor/extensions/masti/config/initializers/global_contants.rb
@@ -0,0 +1,27 @@
+
+
+#Email campain contants
+EMAIL_CAMP ={}
+#EMAIL_CAMP['/email_camp'] = 'camp'
+#EMAIL_CAMP['/email_camp_diff'] = 'diff' 
+EMAIL_CAMP['/email_camp_info'] = 'Infosys' 
+EMAIL_CAMP['/email_camp_tcs'] = 'TCS' 
+EMAIL_CAMP['/email_camp_cts'] = 'CTS' 
+EMAIL_CAMP['/email_camp_polaris'] = 'Polaris' 
+EMAIL_CAMP['/email_camp_eds'] = 'EDS' 
+EMAIL_CAMP['email_camp_wipro'] = 'Wipro' 
+EMAIL_CAMP['/email_camp_flsmidth'] = 'FLSmidth' 
+EMAIL_CAMP['/email_camp_aircel'] = 'Aircel' 
+EMAIL_CAMP['/email_camp_ibm'] = 'IBM' 
+EMAIL_CAMP['email_camp_oracle'] = 'Oracle' 
+EMAIL_CAMP['email_camp_hp'] = 'HP'
+EMAIL_CAMP['/email_camp_ceequence'] = 'Ceequence'
+EMAIL_CAMP['/email_camp_scope']='Scope International'
+
+#Email camp for facebook,orkut,adwords,email_camp,diff
+EMAIL_CAMP_ADD = {}
+EMAIL_CAMP_ADD['/facebook'] = 'facebook'
+EMAIL_CAMP_ADD['/orkut'] = 'orkut'
+EMAIL_CAMP_ADD['/adwords'] = 'adwords'
+EMAIL_CAMP_ADD['/email_camp'] = 'camp'
+EMAIL_CAMP_ADD['/email_camp_diff'] = 'diff'
