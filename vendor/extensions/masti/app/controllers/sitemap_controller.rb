6d6f6e0cf5d77f4280ec4adf7a7f5651a7be8fb3
Ramanavel Selvaraju <ramanainfotech@gmail.com>
2010-10-07 19:13:13 +0530

    xml builder added for deals

--- vendor/extensions/masti/app/controllers/sitemap_controller.rb
+++ vendor/extensions/masti/app/controllers/sitemap_controller.rb
@@ -1,14 +1,36 @@
 class SitemapController < ApplicationController
 
-  session :off
+  
   layout nil 
 
   def sitemap
+    session :off
     headers['Content-Type'] = 'text/xml; charset=utf-8'
     @products = Product.find(:all, :select => 'permalink', :conditions => ['deleted_at is NULL'])
     respond_to do |format|
       format.xml 
     end
   end
-
+  
+  def main_deal
+    unless params[:city_id].nil?
+      session[:city_id] = params[:city_id]
+    end
+    @deal = DealHistory.find(:first, :conditions =>['is_active = ? AND city_id = ?', true , session[:city_id]])
+    @product = Product.find(@deal.product_id)
+     respond_to do |format|
+      format.xml 
+    end
+  end
+  
+  def sub_deal
+    unless params[:city_id].nil?
+      session[:city_id] = params[:city_id]
+    end
+    @deal = DealHistory.find(:first, :conditions =>['is_side_deal = ? AND city_id = ?', true , session[:city_id]])
+    @product = Product.find(@deal.product_id)
+    respond_to do |format|
+      format.xml 
+    end  
+  end
 end
