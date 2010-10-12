cc4127e00e4d6a648df3babbe4eb310e9faced3f
Ramanavel Selvaraju <ramanainfotech@gmail.com>
2010-10-12 11:35:56 +0530

    paypal added

--- vendor/extensions/masti/config/routes.rb
+++ vendor/extensions/masti/config/routes.rb
@@ -59,7 +59,7 @@ map.email_camp_scope '/email_camp_scope', :controller => "home", :action => "ind
 map.email_camp_ceequence '/email_camp_ceequence', :controller => "home", :action => "index"
 map.email_camp_hcl '/email_camp_hcl', :controller => "home", :action => "index"
 map.email_camp_steria '/email_camp_steria', :controller => "home", :action => "index"
-
+map.email_camp_paypal '/email_camp_paypal', :controller => "home", :action => "index"
 #city path
 map.chennai '/chennai', :controller=>'home', :action=>'index', :city_id => 1
 map.hyderabad '/hyderabad', :controller=>'home', :action=>'index', :city_id => 5
