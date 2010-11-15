20405512c6b7f84b8b6d75108a69d674e0741b9c
Ramanavel Selvaraju <ramanainfotech@gmail.com>
2010-11-15 12:17:51 +0530

    hover js corrected

--- vendor/extensions/masti/public/javascripts/new_js/all.js
+++ vendor/extensions/masti/public/javascripts/new_js/all.js
@@ -103,11 +103,15 @@ jQuery(document).ready(function(){
 		//Following events are applied to the subnav itself (moving subnav up and down)
 		jQuery(this).parent().find("ul.subnav").slideDown('fast').show(); //Drop down the subnav on click
 
-		jQuery("ul.topnav").hover(function() {
+		jQuery("#topnav1").hover(function() {
 		}, function(){	
 			jQuery(this).parent().find("ul.subnav").slideUp('slow'); //When the mouse hovers out of the subnav, move it back up
 		});
-
+		jQuery("#topnav2").hover(function() {
+		}, function(){	
+			jQuery(this).parent().find("ul.subnav").slideUp('slow'); //When the mouse hovers out of the subnav, move it back up
+		});
+		
 		//Following events are applied to the trigger (Hover events for the trigger)
 		}).hover(function() { 
 			jQuery(this).addClass("subhover"); //On hover over, add class "subhover"
